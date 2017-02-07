e4677115d852b36ef67906f97fddfacdb4b97dae
Devon Bray <dev@esologic.com>
2017-02-06 22:59:19 -0500

    new main

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ right_arduino = Module(network, 2)
 
 print "Starting Handshake"
 
+handshake_count = 0
+
 while True:
 
     left_response_message = left_arduino.send_message(GetStatus())
     right_response_message = right_arduino.send_message(GetStatus())
 
-    print "L: " + "b: " + str(left_response_message.d0) + " p: " + str(left_response_message.d1) + " R: " + "b: " + str(right_response_message.d0) + " p: " + str(right_response_message.d1)
+    print "Handshakes: " + str(handshake_count) + " L: " + "b: " + str(left_response_message.d0) + " p: " + str(left_response_message.d1) + " R: " + "b: " + str(right_response_message.d0) + " p: " + str(right_response_message.d1)
+
+    handshake_count += 1
